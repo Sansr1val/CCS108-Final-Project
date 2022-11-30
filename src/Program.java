01a98c95f7b391ee877407f459d4db8037a11f33
Sansr1val <110670707+Sansr1val@users.noreply.github.com>
2022-11-30 16:37:55 +0800

    Initial

--- src/Program.java
+++ src/Program.java
@@ -1,4 +1,22 @@
 
 public class Program {
+	private static int idCounter=1;
+	private int id;
+	private String name;
+	private Curriculum curriculum;
+	
+	public Program(String name, Curriculum curriculum){
+		this.name = name;
+		this.curriculum = curriculum;
+		this.id = idCounter;
+		idCounter++;
+	}
+	
+	public void GetFullDetails() {
+		System.out.println("ID: "+id);
+		System.out.println("Program Name: "+name);
+		System.out.println("Curriculum ID: ");//+curriculum.getID();
+	}
+	
 
 }
